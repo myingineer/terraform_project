7ed677fa253745e3ffd014b3c877624ddc9190f0
myingineer <e.alexander190.com>
2025-02-21 09:04:21 +0100

    comments for explanation

--- modules/cloudwatch/outputs.tf
+++ modules/cloudwatch/outputs.tf
@@ -1,3 +1,6 @@
+# Description: This file contains the output variables for the cloudwatch module.
+
+# Output the name of the instance profile created for the CloudWatch agent
 output "cloudwatch_profile_name" {
   value = aws_iam_instance_profile.cloudwatch_instance_profile.name
 }
--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,14 +3,14 @@
 ## Set up the launch configuration for the EC2 instances
 resource "aws_launch_template" "prod_lt" {
   name_prefix   = "prod-lt"
-  image_id      = "ami-04b4f1a9cf54c11d0" 
-  instance_type = "t2.micro"
+  image_id      = "ami-04b4f1a9cf54c11d0"  # Amazon Ubuntu 20.04
+  instance_type = "t2.micro" # Small instance type
   iam_instance_profile {
     name = var.iam_instance_profile
-  }
-  key_name      = "test_key"
+  } # Instance profile for the CloudWatch agent
+  key_name      = "test_key" # Key pair for SSH access
 
-  vpc_security_group_ids = [ var.ec2_sg_id ]
+  vpc_security_group_ids = [ var.ec2_sg_id ] # Security group for the EC2 instance
 
   user_data = base64encode(<<-EOF
               #!/bin/bash
@@ -20,7 +20,7 @@ resource "aws_launch_template" "prod_lt" {
   )
 
   tag_specifications {
-    resource_type = "instance"
+    resource_type = "instance" # Tag the instance
 
     tags = {
       Name = "prod_ec2"
@@ -30,18 +30,18 @@ resource "aws_launch_template" "prod_lt" {
 
 ## Set up the autoscaling group for the EC2 instances
 resource "aws_autoscaling_group" "prod_asg" {
-  count = length(var.private_subnets_id)
+  count = length(var.private_subnets_id) # Create an autoscaling group for each private subnet
   launch_template {
     id      = aws_launch_template.prod_lt.id
     version = "$Latest"
-  }
+  } # Use the launch template for the autoscaling group
 
   vpc_zone_identifier = [ var.private_subnets_id[count.index] ]
 
   min_size             = 1   # Start with 1 instance
   max_size             = 4   # Can scale up to 4 instances
   desired_capacity     = 1   # Initial instance count set to 1
-  health_check_type    = "ELB"
+  health_check_type    = "ELB" # Health check type
   health_check_grace_period = 300
 
   tag {
